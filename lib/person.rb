33523b76e955305cf4d1fcd164fd3f1efd504b26
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-04-30 15:48:25 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,22 +1,22 @@
 class Person
   def init(name)
-    @name = name 
+    @name = name
   end
   def name
     @name
   end
   def name=(name)
-    @name = name 
+    @name = name
   end
 
 
   def intj(job)
-    @job = job 
+    @job = job
   end
   def job
     @job
   end
   def job=(job)
-    @job = job 
+    @job = job
   end
-  
+end
